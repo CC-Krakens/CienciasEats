0d4bbafdf29804331762187e4494c9b51273b88d
Mario Andrés Rosales Peña <mariorosales@ciencias.unam.mx>
2024-05-18 22:53:48 -0600

    Pantalla de inicio añadida

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './components/Login';
 import Home from './components/Home';
+import Inicio from './components/Inicio';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Inicio />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
---
+++ react_app/src/components/Inicio.js
@@ -0,0 +1,22 @@
+// src/components/Inicio.js
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './Inicio.css';
+
+import axios from 'axios';
+
+function Inicio() {
+  const navigate = useNavigate();
+    const location = useLocation();
+
+  return (
+    <div className="home-container">
+      <h1>Ciencias eats</h1>
+      <p>Bienvenido.</p>
+      <button onClick={() => navigate('/login')}>Log In</button>
+      <button onClick={() => navigate('/home')}>Registrarse</button>
+    </div>
+  );
+}
+
+export default Inicio;
