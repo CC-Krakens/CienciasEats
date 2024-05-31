04f1aa6b09420ae9308e87f867f569d94e17b381
Mario Andrés Rosales Peña <mariorosales@ciencias.unam.mx>
2024-05-31 07:38:51 -0600

    Organización de carpetas actualizada

---
+++ react_app/src/App.js
@@ -0,0 +1,26 @@
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './components/Login';
+import Home from './components/Home';
+import Inicio from './components/Inicio';
+import Registro from './components/Registro';
+import Home_Comprador from './components/Home_Comprador';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/home_comprador" element={<Home_Comprador />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;Ap
--- react_app/src/components/Home_Vendedor.js
+++ react_app/src/components/Home_Vendedor.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import "./App.css";
+import './styles.css';
 
 
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
